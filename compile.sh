0631918912128c7f11c65c2e4e398a512fe9bc63
Jim White <james.paul.white@gmail.com>
2014-02-20 19:30:55 -0800

    Remove dependency on my environment for compiling against Groovy jar.

--- compile.sh
+++ compile.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 GROOVY_BIN=`which groovy`
-GROOVY_ALL_JAR=`find "$GROOVY_BIN/../../embeddable" -type f ! -name "*-indy.jar"`
+GROOVY_ALL_JAR=`find "$GROOVY_BIN/../../embeddable" -type f ! -name "*-indy.jar" | head -n 1`
 javac -cp "$GROOVY_ALL_JAR" -d classes java-src/*
